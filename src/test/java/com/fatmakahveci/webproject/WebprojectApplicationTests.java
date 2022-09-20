0e26a2ba214141dfafbdb6623ab1cfa2e97f399f
fatmakahveci <fatmaba@gmail.com>
2022-09-20 11:33:43 +0100

    missing files are added

---
+++ src/test/java/com/fatmakahveci/webproject/WebprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatmakahveci.webproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
