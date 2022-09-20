72264a04ba642745bde6d7f2767b39ed06c8ec53
fatmakahveci <fatmaba@gmail.com>
2022-09-20 11:33:43 +0100

    missing files are added

---
+++ src/main/java/com/fatmakahveci/webproject/WebprojectApplication.java
@@ -0,0 +1,13 @@
+package com.fatmakahveci.webproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebprojectApplication.class, args);
+	}
+
+}
