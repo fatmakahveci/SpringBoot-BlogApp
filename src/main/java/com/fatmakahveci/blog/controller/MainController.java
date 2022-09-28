d005f96e31ea31b11b0a8fec2cabb0a6985b6c7b
fatmakahveci <fatmaba@gmail.com>
2022-09-28 11:26:07 +0100

    buttons are working but fix is required

--- src/main/java/com/fatmakahveci/blog/controller/MainController.java
+++ src/main/java/com/fatmakahveci/blog/controller/MainController.java
@@ -5,19 +5,26 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.fatmakahveci.blog.model.Post;
+import com.fatmakahveci.blog.model.Tag;
 import com.fatmakahveci.blog.service.PostService;
+import com.fatmakahveci.blog.service.TagService;
 
 @Controller
 public class MainController {
     private PostService postService;
+    private TagService tagService;
 
     @Autowired
-    public MainController(PostService postService) {
+    public MainController(PostService postService, TagService tagService) {
         this.postService = postService;
+        this.tagService = tagService;
     }
 
     @GetMapping({"/",""})
@@ -26,5 +33,31 @@ public class MainController {
         var params = new HashMap<String, List<Post>>();
         params.put("posts", posts);
         return new ModelAndView("index", params);
+    }
+
+    @GetMapping(path = "/post")
+    public String postForm(Model model) {
+        model.addAttribute("post", new Post());
+        return "post";
+    }
+
+    @PostMapping("/post")
+    public String greetingSubmit(@ModelAttribute Post post, Model model) {
+      model.addAttribute("post", post);
+      postService.save(post);
+      return "post";
+    }
+
+    @GetMapping(path = "/tag")
+    public String tagForm(Model model) {
+        model.addAttribute("tag", new Tag());
+        return "tag";
+    }
+
+    @PostMapping("/tag")
+    public String greetingSubmit(@ModelAttribute Tag tag, Model model) {
+      model.addAttribute("tag", tag);
+      tagService.save(tag);
+      return "tag";
     }
 }
