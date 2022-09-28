47dfe86239007532659a95c3b7075b4de2c8728b
fatmakahveci <fatmaba@gmail.com>
2022-09-28 23:14:14 +0100

    Methods related to PostController are taken from MainController.

--- src/main/java/com/fatmakahveci/blog/controller/MainController.java
+++ src/main/java/com/fatmakahveci/blog/controller/MainController.java
@@ -3,11 +3,8 @@ package com.fatmakahveci.blog.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.fatmakahveci.blog.model.Post;
 import com.fatmakahveci.blog.service.PostService;
 
 @Controller
@@ -24,19 +21,5 @@ public class MainController {
         ModelAndView mav = new ModelAndView("index");
 		mav.addObject("posts", postService.findAll());
         return mav;
-    }
-
-    @GetMapping("/addNewPost")
-    public ModelAndView addNewPost() {
-        ModelAndView mav = new ModelAndView("post_form");
-        Post newPost = new Post();
-        mav.addObject("post", newPost);
-        return mav;
-    }
-    
-    @PostMapping("/savePost")
-    public String savePost(@ModelAttribute Post post) {
-        postService.save(post);
-        return "redirect:/";
     }
 }
--- src/main/java/com/fatmakahveci/blog/controller/PostController.java
+++ src/main/java/com/fatmakahveci/blog/controller/PostController.java
@@ -3,14 +3,13 @@ package com.fatmakahveci.blog.controller;
 import com.fatmakahveci.blog.model.Post;
 import com.fatmakahveci.blog.service.PostService;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -24,19 +23,22 @@ public class PostController {
         this.postService = postService;
     }
 
-    @PostMapping
-    public void savePost(@RequestBody Post post) {
-        postService.save(post);
-    }
-
     @GetMapping(path = "/{id}")
     public Post viewPostById(@PathVariable Integer id) {
         return postService.findById(id);
     }
 
-    @GetMapping(path = {"","/"})
-    public List<Post> viewAllPosts() {
-        return postService.findAll();
+    @GetMapping("/addNewPost")
+    public ModelAndView addNewPost() {
+        ModelAndView mav = new ModelAndView("post_form");
+        mav.addObject("post", new Post());
+        return mav;
+    }
+    
+    @PostMapping("/savePost")
+    public ModelAndView savePost(@ModelAttribute Post post) {
+        postService.save(post);
+        return new ModelAndView("redirect:/");
     }
 
     // @TODO: write editPost method
