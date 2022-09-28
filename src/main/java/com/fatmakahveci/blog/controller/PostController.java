671b9ff9a62b069d2ed6b71e6c65f2ae7121d6a8
fatmakahveci <fatmaba@gmail.com>
2022-09-28 11:26:07 +0100

    buttons are working but fix is required

--- src/main/java/com/fatmakahveci/blog/controller/PostController.java
+++ src/main/java/com/fatmakahveci/blog/controller/PostController.java
@@ -3,12 +3,10 @@ package com.fatmakahveci.blog.controller;
 import com.fatmakahveci.blog.model.Post;
 import com.fatmakahveci.blog.service.PostService;
 
-import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,14 +37,6 @@ public class PostController {
     @GetMapping(path = {"","/"})
     public List<Post> viewAllPosts() {
         return postService.findAll();
-    }
-
-    @GetMapping(path = "/createNewPost")
-    public ModelAndView createNewPost() {
-        var posts = postService.findAll();
-        var params = new HashMap<String, List<Post>>();
-        params.put("posts", posts);
-        return new ModelAndView("post_form", params);
     }
 
     // @TODO: write editPost method
