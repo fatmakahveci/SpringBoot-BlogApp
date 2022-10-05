6536b5e74555dd694ca77a9da0f87415bd16a072
fatmakahveci <fatmaba@gmail.com>
2022-10-05 15:33:32 +0100

    fix

--- src/test/java/com/fatmakahveci/blog/BlogApplicationTests.java
+++ src/test/java/com/fatmakahveci/blog/BlogApplicationTests.java
@@ -1,7 +1,7 @@
 package com.fatmakahveci.blog;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.context.SpringBootTest;
 
 // @SpringBootTest
 class BlogApplicationTests {
