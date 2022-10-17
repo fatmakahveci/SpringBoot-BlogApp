7ab9a6999542cc7791d15fe87da2a9d58aa9226a
fatmakahveci <fatmaba@gmail.com>
2022-10-17 10:28:50 +0100

    redundant parts are deleted

--- src/main/java/com/fatmakahveci/blog/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.fatmakahveci.blog;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(Integer id) {
-        super("Could not find user " + id);
-    }
-}
