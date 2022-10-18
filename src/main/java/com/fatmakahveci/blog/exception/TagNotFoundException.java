4d71d66bb085736e1486d6c81e59b0a664573d7c
fatmakahveci <fatmaba@gmail.com>
2022-10-18 16:14:14 +0100

    fix

--- src/main/java/com/fatmakahveci/blog/exception/TagNotFoundException.java
+++ src/main/java/com/fatmakahveci/blog/exception/TagNotFoundException.java
@@ -1,4 +1,4 @@
-package com.fatmakahveci.blog;
+package com.fatmakahveci.blog.exception;
 
 public class TagNotFoundException extends RuntimeException {
     public TagNotFoundException(Integer id) {
